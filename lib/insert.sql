281b46d5b57945188eb06771f67fd40d7ef4cdd5
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-09-08 16:19:27 +0300

    Completed the lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 9, "M", "Chocolate", "none", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 6, "M", "Brown", "none", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "White", "none", "false");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Dark Brown", "none", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "Grey", "none", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Green", "none", "false");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "Black", "none", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
+
+SELECT * FROM bears;
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,36 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT bears.name, bears.age FROM bears WHERE bears.sex = "F";
+"""
+
+select_all_male_bears_return_name_and_age = """
+    SELECT bears.name, bears.age FROM bears WHERE bears.sex = "M":
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT bears.name FROM bears ORDER BY bears.name;
+"""
+
+select_all_bears_names_and_ages_that_are_alive = """
+    SELECT bears.name, bears.age FROM bears WHERE bears.alive = true;
+"""
+
+select_all_bears_names_and_ages_that_are_alive_and_order_oldest_to_youngest = """
+    SELECT bears.name, bears.age FROM bears WHERE bears.alive = true ORDER BY bears.age DESC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT bears.name, bears.age FROM bears WHERE bears.alive = true ORDER BY bears.age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;
 """
+
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT bears.name, bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;
 """
+
+# select_average_age_of_all_bears = """
+#     SELECT AVG(bears.age) FROM bears;
+# """
+
